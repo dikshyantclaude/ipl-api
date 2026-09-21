a7dc1fc77a5dd793aa648ebadcdec21e7af60fbb
dikshyantclaude <diksyant12@gmail.com>
2026-09-21 00:59:41 -0400

    Added Something

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ def root():
 @app.get("/ipl/teams")
 def return_unique_teams():
     unique_teams = list(set(ipl['Team1']))
-    return {"Teams": unique_teams}
-     
+    return {'Teams':unique_teams}  
+
+@app.get("/ipl/faceoff/{team1_name}/{team2_name}")
+def team_1_vs_team_2(team1_name:str, team2_name:str):
+    matches_played = ipl[((ipl['Team1'] == team1_name) & (ipl['Team2'] == team2_name) ) | (ipl['Team1']==team2_name) & (ipl['Team2'] == team1_name) ]
+    matches_count = matches_played.shape[0]
+    if matches_count == 0:
+        return {'Message':'No matches played between these two teams'}
+    matches_won_by_first = matches_played[matches_played['WinningTeam'] == team1_name]
+    matches_won_by_second = matches_played[matches_played['WinningTeam'] == team2_name]
+    first_team_matches_won = matches_won_by_first.shape[0]
+    second_team_matches_won = matches_won_by_second.shape[0]
+    return {'Matches Played':matches_count, team1_name+' Wins':first_team_matches_won, team2_name+' Wins':second_team_matches_won}
     
